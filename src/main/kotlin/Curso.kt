ee8244e3de27f28a84c57fa2b02ff4d475c20df4
Renan Cleber <renancorredato@hotmail.com>
2021-06-13 12:02:36 -0300

    Parte E

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -1,7 +1,11 @@
 class Curso(
     val nome: String,
-    val codigoCurso: Int
+    val codigoCurso: Int,
+    val qtdMaximaAluno:Int
 ) {
+    lateinit var professoresTitulares: ProfessoresTitulares
+    lateinit var professoresAdjuntos:ProfessoresAdjuntos
+    var listaAlunosMatriculados = mutableListOf<Aluno>()
 
     override fun equals(other: Any?): Boolean {
         return (other as? Curso)?.codigoCurso == this.codigoCurso
