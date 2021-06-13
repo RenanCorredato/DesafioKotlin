1899a52988f2bb88cfddaa513320aef3051944d2
Renan Cleber <renancorredato@hotmail.com>
2021-06-13 12:30:36 -0300

    Parte H

---
+++ src/main/kotlin/DigitalHouseManager.kt
@@ -0,0 +1,7 @@
+class DigitalHouseManager(
+    var listaAluno: MutableList<Aluno> = mutableListOf<Aluno>(),
+    var listaProfessores: MutableList<Professor> = mutableListOf<Professor>(),
+    var listaCursos: MutableList<Curso> = mutableListOf<Curso>(),
+    var listaMatriculas: MutableList<Matricula> = mutableListOf<Matricula>()
+) {
+}
