d3673b40e040293b72bd40f63d0ca76864ca797e
Renan Cleber <renancorredato@hotmail.com>
2021-06-13 12:47:05 -0300

    Parte H

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -1,23 +1,27 @@
 class Curso(
     val nome: String,
     val codigoCurso: Int,
-    val qtdMaximaAluno:Int
+    val qtdMaximaAluno: Int
 ) {
     lateinit var professoresTitulares: ProfessoresTitulares
-    lateinit var professoresAdjuntos:ProfessoresAdjuntos
+    lateinit var professoresAdjuntos: ProfessoresAdjuntos
     var listaAlunosMatriculados = mutableListOf<Aluno>()
 
     override fun equals(other: Any?): Boolean {
         return (other as? Curso)?.codigoCurso == this.codigoCurso
     }
 
-    fun adicionarUmAluno(umAluno: Aluno): Boolean{
-       return if (listaAlunosMatriculados.size < qtdMaximaAluno) {
-           listaAlunosMatriculados.add(umAluno)
-           return true
-       }else{
-           return false
+    fun adicionarUmAluno(umAluno: Aluno): Boolean {
+        return if (listaAlunosMatriculados.size < qtdMaximaAluno) {
+            println("Aluno adicionado $listaAlunosMatriculados.add(umAluno)")
+            return true
+        } else {
+            return false
+        }
+    }
 
-       }
+    fun excluirAluno(umAluno: Aluno) {
+        listaAlunosMatriculados.remove(umAluno)
+        println("Aluno removido")
     }
 }
